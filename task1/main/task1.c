261b87f467c6a7f8c61429d7e665adffc1aa445e
sai25goud <saikiran25goud@gmail.com>
2022-01-24 16:44:21 +0530

    day4 assignment

--- task1/main/task1.c
+++ task1/main/task1.c
@@ -2,25 +2,41 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-TaskHandle_t xHandleTaskLog;
+TaskHandle_t xHandleTaskLog1;
+TaskHandle_t xHandleTaskLog2;
 
-void TaskLog(void *pvParameters)
+void TaskLog1(void *pvParameters)
 {
   while(1)
+  {
   printf("TaskLog1\n");
-  vTaskDelete(xHandleTaskLog);
+  vTaskDelete(xHandleTaskLog1);
+  }
+}
+
+void TaskLog2(void *pvParameters)
+{
+  while(1)
+  {
   printf("TaskLog2\n");
-  vTaskDelete(xHandleTaskLog);
+  vTaskDelete(xHandleTaskLog2);
+  }
 }
 
 void app_main()
 {
- BaseType_t result;
- result = xTaskCreate(TaskLog,"TaskLog1",2048,NULL,5, &xHandleTaskLog);
- result = xTaskCreate(TaskLog,"TaskLog2",2048,NULL,10, &xHandleTaskLog);
-    if(result == pdPASS)
-    {
-        printf("TaskLog created\n");
 
+ BaseType_t result1,result2;
+ result1 = xTaskCreate(TaskLog1,"TaskLog1",2048,NULL,5, &xHandleTaskLog1);
+ result2 = xTaskCreate(TaskLog2,"TaskLog2",2048,NULL,10, &xHandleTaskLog2);
+ 
+    if(result1 == pdPASS)
+    {
+        printf("TaskLog1 created\n");
+    }
+    
+    if(result2 == pdPASS)
+    {
+        printf("TaskLog2 created\n");
     }
 }
